f67184334ef51926f89496a0d72a3610a96c276b
ZackyFaishal <zacky.faishal.tif421@polban.ac.id>
2022-02-27 22:55:15 +0700

    Challenge week 4

---
+++ Pertemuan 4/UpindanIpin/src/Item.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author faish
+ */
+public class Item {
+    private String name;
+    private Item() {
+    name = "Ipin";
+    }
+    public Item(String name) {
+    this();
+    System.out.println(this.name);
+    }    
+}
