2838b939561f8927291d2dbe1e024c3089b2b033
ZackyFaishal <zacky.faishal.tif421@polban.ac.id>
2022-04-10 21:59:44 +0700

    Latihan 1 week 9

---
+++ Pertemuan 9/Latihan 1/dynamicpolymorphism/src/main/java/dynamic/Animal.java
@@ -0,0 +1,11 @@
+package dynamic;
+
+/**
+ *
+ * @author Zacky Faishal Abror
+ */
+public class Animal {
+    public void sound(){
+        System.out.println("Animal is making a sound");
+    }
+}
---
+++ Pertemuan 9/Latihan 1/dynamicpolymorphism/src/main/java/dynamic/Horse.java
@@ -0,0 +1,19 @@
+package dynamic;
+
+/**
+ *
+ * @author Zacky Faishal Abror
+ */
+public class Horse extends Animal {
+    @Override
+    public void sound(){
+        System.out.println("Neigh");
+    }
+    
+    public static void main(String args[]){
+        Animal obj = new Horse();
+        obj.sound();
+    }
+}
+
+
